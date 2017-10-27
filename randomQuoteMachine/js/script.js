f586624d6416723823c55c9b3c7626ba354e4f2b
João <fernandesjvb@gmail.com>
2017-10-27 00:25:40 -0200

    implements the random quote machine

---
+++ randomQuoteMachine/js/script.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+
+	$(window).on('load', function() {
+		getQuote();	
+	});
+
+	function getQuote(){
+
+		$('#quotediv').hide();
+		$('#loadingDiv').show();
+
+		$.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en", 
+			function(data) {
+	  			$("#quote").text(data.quoteText);
+
+	  			if(data.quoteAuthor.trim() === ""){
+	  				data.quoteAuthor = "Author Unknown";
+	  			}
+
+	  			$("#author").text(" — " + data.quoteAuthor);
+	  			console.log(data);
+
+	  			$('.button-primary').attr('href', "https://twitter.com/intent/tweet?text=" + data.quoteText + " — " + data.quoteAuthor);
+				$('#loadingDiv').hide();
+				$('.pageloading').fadeOut('slow');
+				$('#quotediv').show();
+
+			});
+	}
+
+	$("#new").on('click', function(){
+		getQuote();
+	});
+
+});
