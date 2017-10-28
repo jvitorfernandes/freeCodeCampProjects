1df58876e5989d2b2c14efc6230059ee975b7dee
João <fernandesjvb@gmail.com>
2017-10-28 02:27:17 -0200

    loading spinner and message to user allow location

--- localWeather/js/script.js
+++ localWeather/js/script.js
@@ -9,23 +9,29 @@ function getWeatherInfo(latitude, longitude){
 
 		temp = data.main.temp;
 
+		
 		$("#location").append(data.name + ", " + data.sys.country);
 		$("#description").text(data.weather[0].description);
 		$('#icon').attr("src", data.weather[0].icon);
 		$('#temp').text(data.main.temp + " ºC");
+		$('.pageloading').hide();
+		$('#box').fadeIn();
+
+
 	});
 }
 
 function getLocation(){
-    console.log('hi');
+
 	if(navigator.geolocation){
-        console.log('hi');
+
 		navigator.geolocation.getCurrentPosition(function(position){
             
 			var latitude = parseFloat(position.coords.latitude);
 			var longitude = parseFloat(position.coords.longitude);
             
-            console.log(latitude, longitude);
+            $('#message').hide();
+            $('.pageloading').show();
 
 			getWeatherInfo(latitude, longitude);
 
