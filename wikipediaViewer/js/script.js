ee5944e0c9fdfd2542a76a947b313b6ba3f28cf6
João <fernandesjvb@gmail.com>
2017-11-03 03:39:30 -0200

    finish it all

--- wikipediaViewer/js/script.js
+++ wikipediaViewer/js/script.js
@@ -3,13 +3,23 @@ $( document ).ready(function() {
 	$(window).on('load', function() {
 		$('#box').show();
 		$('.pageloading').fadeOut('slow');
-		search();	
 	});
 
-	$(".button-primary").on('click', function(){
-		$("#start").hide();
-        search($('#pesquisaInput').val());
+	$("#pesquisaBotao").on('click', function(){
+        callSearch($('#pesquisaInput').val());
 	});
+    
+    $("#pesquisaBotao2").on('click', function(){
+        callSearch($('#pesquisaInput2').val());
+	});
+
+    
+    function callSearch(term){
+        $("#start").hide();
+        $('.pageloading').show();
+        search(term);
+        console.log('hi');
+    }
 
 	function search(term){
         
@@ -21,7 +31,22 @@ $( document ).ready(function() {
              type: 'get',
              headers: { 'Api-User-Agent': 'Example/1.0' },
              success: function(data) {
-               console.log(data);
+                 
+                 var resultados  = data.query.search;
+                 
+                 $(".resultados").empty();
+                 $(".resultados").append('<hr>')
+                 for(i=0; i<resultados.length; i++){
+                     $(".resultados").append('<div class=resultado>');
+                     $('.resultado:last-child').html('<a class="link"><h6><strong>' 
+                                                     + resultados[i].title + '</strong></h6></a><p>' + resultados[i].snippet + '</p>');
+                     $('a:last').attr('href', 'https://en.wikipedia.org/wiki/' + resultados[i].title);
+                     $('a:last').attr('target', '_blank');
+                     $(".resultados").append('<hr>');
+                 }
+                 $('.pageloading').hide();
+                 $('#search').fadeIn();
+                 
             }
              
          })
